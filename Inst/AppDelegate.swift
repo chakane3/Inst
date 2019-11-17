476ca08e68362aea029e263e8b35d4f4aba1934d
Chakane Shegog <chakanezshegog@gmail.com>
2019-11-17 03:47:40 -0500

    okay

--- Inst/AppDelegate.swift
+++ Inst/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Initialize Parse
+              // Set applicationId and server based on the values in the Heroku settings.
+              Parse.initialize(
+                  with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                      configuration.applicationId = "Inst"
+                      configuration.server = "https://mysterious-badlands-83646.herokuapp.com/parse"
+                  })
+              )
+        
+
         return true
     }
 
